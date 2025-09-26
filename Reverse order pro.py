961b40b12f4a23522f992c94b4088d344716fd3a
AamirCode2 <aamirlovesart@gmail.com>
2025-09-26 17:18:10 +1000

    Delete Reverse order pro.py

--- Reverse order pro.py
+++
@@ -1,7 +0,0 @@
-num = str(input("Enter a number you want to find how many digits are in it: "))
-n = 0          
-
-while n != len(num):
-    n = n+1
-
-print("number of digits in {0} is {1}.".format(num, n))
